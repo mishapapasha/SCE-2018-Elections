f4976cc089eed0cb78f4b5fd59c4b0774bccbdb3
JohnVituli <webuyingabroad@gmail.com>
2017-12-08 20:23:59 +0200

    TEST jenkins

--- app/models.py
+++ app/models.py
@@ -5,8 +5,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(120), index=True, unique=False)
     last_name = db.Column(db.String(120), index=True, unique=False)
-    user_id = db.Column(db.Integer ,index=True,unique=True)
-    voted = db.Column(db.BOOLEAN , index=True,unique=False)
+    user_id = db.Column(db.Integer ,index=True, unique=True)
+    voted = db.Column(db.BOOLEAN , index=True, unique=False)
 
     def __init__(self, first_name, last_name, user_id):
         self.first_name = first_name
